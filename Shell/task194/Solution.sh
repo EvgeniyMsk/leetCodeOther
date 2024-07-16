e95179b9c5cf0921646b48b5b711b9548ab48216
lev <lev@ou.shtks>
2024-07-16 18:23:35 +0300

    194 solved

---
+++ Shell/task194/Solution.sh
@@ -0,0 +1,17 @@
+# manual https://www.grymoire.com/Unix/Awk.html
+awk '
+{
+for (i = 1; i <= NF; i++) {
+  if (FNR == 1) {
+    t[i] = $i;
+  } else {
+    t[i] = t[i] " " $i
+  }
+}
+}
+END {
+  for (i = 1; t[i] != ""; i++) {
+    print t[i];
+  }
+}
+' file.txt
